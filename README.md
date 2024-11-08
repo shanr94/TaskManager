8fc23240e5567cbcdee8b69e3344edbf46bb690b
Shantanu Rastogi <codershantanu94@gmail.com>
2024-11-09 02:14:26 +0530

    Task Manager using LocalStorage
    Add Task Manager Application with Local Storage Persistence
    
    - Developed a user-friendly, responsive task manager using HTML, CSS, and JavaScript
    - Enabled data persistence by utilizing browser's local storage, allowing tasks to be retained even after browser shutdown
    - Enhanced user experience with a simple, intuitive UI for managing tasks effectively

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TaskManager
- Task Manager using LocalStorage
+
+A user-friendly and responsive web application built with HTML, CSS, and JavaScript. This task manager leverages browser local storage to save tasks, ensuring that data persists even if the browser is closed or the computer is restarted. The app allows users to manage their tasks seamlessly without losing data.
---
+++ script.js
@@ -0,0 +1,93 @@
+let form = document.querySelector("form");
+//adding event listner on form submit
+form.addEventListener("submit", (e) => {
+  let duplicateTask = false;
+  //prevent default behaviour of reloading after submission of form
+  e.preventDefault();
+  let subject = e.target.subject.value;
+  let priority = e.target.priority.value;
+  let dueDate = e.target.dueDate.value.replace("T", " ");
+  let details = e.target.details.value;
+  let txtColor = "";
+
+  //setting text color based on priority
+  if (priority == "high") {
+    txtColor = 'style="color: red"';
+  } else if (priority == "medium") {
+    txtColor = 'style="color: yellow"';
+  } else if (priority == "low") {
+    txtColor = "";
+  }
+
+  //getting data from local storage as JSON and converting it in Array
+  let allTasks = JSON.parse(localStorage.getItem("taskList")) ?? [];
+
+  //checking for duplicate entery
+  for (let task of allTasks) {
+    if (task.subject == subject && task.dueDate == dueDate) {
+      duplicateTask = true;
+      break;
+    }
+  }
+
+  //if duplicate found alert user else create task
+  if (duplicateTask) {
+    alert("Task alraedy exist!");
+  } else {
+    allTasks.push({
+      subject: subject,
+      priority: priority,
+      dueDate: dueDate,
+      details: details,
+      txtColor: txtColor,
+    });
+
+    //pushing data in Local Storage
+    localStorage.setItem("taskList", JSON.stringify(allTasks));
+    //caling displayHandler function to show all tasks to user on browser screen
+    displayHandler();
+  }
+});
+
+function displayHandler() {
+  //getting data from local storage as JSON and converting it in Array
+  let allTasks = JSON.parse(localStorage.getItem("taskList")) ?? [];
+
+  let finalList = [];
+
+  //dynamically adding new task to existing taskList and storing in new array
+  allTasks.forEach((elm, i) => {
+    finalList.push(
+      `
+            <div class="task">
+                <span class="deleteIcon" onclick=deleteHandler(${i})> &times;</span>
+                <h3>Subject: <span>${elm.subject}</span></h3>
+                <h3>Priority: <span ${
+                  elm.txtColor
+                }>${elm.priority.toUpperCase()}</span></h3>
+                <h3>Due Date: <span>${elm.dueDate}</span></h3>
+                <h3>Task Details: </h3>
+                <div class="taskDetails">${elm.details}</div>
+            </div>
+            `
+    );
+  });
+
+  //updating DOM to show all tasks to user
+  document.getElementById("pendingTasks").innerHTML = finalList;
+
+  //form reset
+  form.reset();
+}
+
+function deleteHandler(index) {
+  //getting data from local storage as JSON and converting it in Array
+  let allTasks = JSON.parse(localStorage.getItem("taskList")) ?? [];
+  //removing the clicked task from task list and pushing the reamining list to local storage
+  allTasks.splice(index, 1);
+  localStorage.setItem("taskList", JSON.stringify(allTasks));
+
+  displayHandler();
+}
+
+displayHandler();
